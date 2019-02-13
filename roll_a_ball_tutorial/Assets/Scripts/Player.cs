d6fc1f1d9a85593dcd1b633e0ae8d6454049f048
cole <coleswany@gmail.com>
2019-02-13 10:18:02 -0500

    Uploading completed Roll-a-ball tutorial as per the video tutorials

--- roll_a_ball_tutorial/Assets/Scripts/Player.cs
+++ roll_a_ball_tutorial/Assets/Scripts/Player.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
     public float speed = 1;
+
+    public Text countText;
+
+    public Text winText;
+
+    private int count;
+
+    void Start () {
+      count = 0;
+      SetCountText ();
+      winText.text = "";
+    }
 
 	// move the player
 	private void FixedUpdate () {
@@ -17,4 +30,19 @@ public class Player : MonoBehaviour {
         GetComponent<Rigidbody>().AddForce(move*speed);
 
 	}
+  void OnTriggerEnter(Collider other){
+
+      if (other.gameObject.CompareTag("Pick Up")){
+        other.gameObject.SetActive (false);
+        count = count + 1;
+        SetCountText ();
+      }
+  }
+  void SetCountText() {
+    countText.text = "Count: " + count.ToString ();
+    if (count >= 12){
+      winText.text = "You win!";
+    }
+  }
 }
+//Destroy(other.gameObject);
