91eda630d6d8cb733326c2484c94b50cf8b0562b
ADS\cswany <coleswany@gmail.com>
2019-02-13 12:29:13 -0500

    Added colorful, tilted walls for more mobility; added instantiating, low-gravity cubes to come down periodically and create extra obstacles

---
+++ roll_a_ball_tutorial/Assets/Scripts/Dropper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+
+    public Transform droppercube;
+    private float x = 10;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("TimeWait", 3f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void TimeWait()
+    {
+        Instantiate(droppercube, new Vector3(x, 20, 0), Quaternion.identity);
+        x = x * -1;
+    }
+}
